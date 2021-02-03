d1b69a100f7be518b1d1dc4370124a514990db7c
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-03 17:04:45 +0900

    Revert "モデルを編集"
    This reverts commit 26b65fe1c49a891c2ab47771372f635eb5ef70df.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,10 +3,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  # imageも空で投稿できないように追記
-  validates :text, :image, presence: true
-
-
   def self.search(search)
     if search != ""
       Tweet.where('text LIKE(?)', "%#{search}%")
