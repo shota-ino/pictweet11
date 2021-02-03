62e8eea8620dee74b515af73ac4f3eea40863d55
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 08:45:55 +0900

    nicknameが空の場合の記述
    expect().to matcher()を雛形に、テストの内容に応じて引数やmatcherを変えて記述
    bundle exec rspec spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      # nicknameが空では登録できないテストコードを記述します
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します
     end 
   end
 end
+
+
+# 異常系のモデル単体テストの実装は、以下の流れで進みます。
+
+# 1保存するデータ（インスタンス）を作成する
+# 2作成したデータ（インスタンス）が、保存されるかどうかを確認する
+# 3保存されない場合、生成されるエラーメッセージが想定されるものかどうかを確認する
+
+# presence: tureが正常に機能するか検証するため、バリデーションを実行
+# Userモデルのバリデーションにはpresence: trueが指定されているため、nicknameが空ではDBに保存できないと判断され、valid?メソッドはfalseを返すはず
 
 
 # itメソッドは、describeメソッド同様に、グループ分けを行うメソッドです。
