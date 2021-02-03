0f2397f47d2c05d68122cd765237053cf9277323
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-03 16:49:34 +0900

    コントローラーを編集
    createアクションの中にツイートが保存される場合の条件分岐を追記します。
    @tweetを定義し、valild?メソッドを使用してツイートが保存されなければ、newへ戻る記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,8 +13,15 @@ class TweetsController < ApplicationController
   end
 
   def create
-    # binding.pry
-    Tweet.create(tweet_params)
+    @tweet = Tweet.new(tweet_params)
+    #バリデーションで問題があれば、保存はされず「投稿画面」に戻る
+    if @tweet.valid?
+      @tweet.save
+      redirect_to root_path
+    else
+      #保存されなければ、newに戻る
+      render 'new'
+    end
   end
 
   def destroy
