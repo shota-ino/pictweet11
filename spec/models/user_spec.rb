0c6a80568a84c7f227ad1fdf9e55886b072d30b5
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 08:35:09 +0900

    describeメソッドを用いてテスト内容を記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    # ユーザー新規登録についてのテストコードを記述します  
+  end
 end
+
+
+# describeとは、テストコードのグループ分けを行うメソッドです。
+# 「どの機能に対してのテストを行うか」をdescribeでグループ分けし、その中に各テストコードを記述します。
+# describeにつづくdo~endの間に、さらにdescribeメソッドを記述することで、入れ子構造をとることもできます。
+# describeメソッドを用いて、「ユーザー新規登録」についてのテストを行うことを記述
+
