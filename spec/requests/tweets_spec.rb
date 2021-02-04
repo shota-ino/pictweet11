5d64883f97f7626e0196d78c6699b0dd49cc15a7
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 10:23:12 +0900

    レスポンスに含まれることを確認
    response.body

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -11,10 +11,17 @@ describe TweetsController, type: :request do
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.text)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.image)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+      get root_path
+      # binding.pry
+      expect(response.body).to include('投稿を検索する')
     end
   end
 end
