fc79a16177c6786e1a0827a55b24dededd0b34ce
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 12:28:21 +0900

    サポートモジュールを読み込めるように
    rails_helper.rbのコメントアウトを外す
    モジュールを設定する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -31,6 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include SignInSupport
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -62,3 +63,13 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+
+# 
+
+# rails_helper.rbのコメントアウトを外す　コメントアウトを外して、読み込まれるように
+# モジュールを設定する　RSpec.configureの中に、以下のようにモジュール名を明記
+
+# sign_in(user)というメソッドで、ログインの一通りの流れをまとめました。
+# これまで結合テストコードにあったログインのステップを、sign_in(user)に置き換えましょう。
+
