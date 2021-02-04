70a50d309b24612bc21b188289b621d02817c289
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 09:21:57 +0900

    すべてのexampleを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,19 +6,35 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+    end
+    it 'nicknameが6文字以下であれば登録できる' do
+    end
+    it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+    end
     it 'nicknameが空では登録できない' do
-      @user.nickname = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'emailが空では登録できない' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Email can't be blank"
-    end 
+    end
+    it 'passwordが空では登録できない' do
+    end
+    it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+    end
+    it 'nicknameが7文字以上では登録できない' do
+    end
+    it '重複したemailが存在する場合登録できない' do
+    end
+    it 'passwordが5文字以下では登録できない' do
+    end
   end
 end
 
+
+# :validatableという記述　　ｄeviseによるバリデーションも自動的
+# emailバリデーション	・
+# 存在すること　一意であること　@を含むこと	・
+# passwordバリデーション
+# 存在すること　6文字以上128文字以下であること
 
 
 # 変数を受け渡す際は、beforeに定義する変数はインスタンス変数にする必要があります。
