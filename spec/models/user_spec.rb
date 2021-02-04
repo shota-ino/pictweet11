799af6a475f1e6e485b2904617a5e4238ed56b26
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 09:02:09 +0900

    共通した記述を切り出し
    bundle exec rspec spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.nickname = ''  # nicknameの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Nickname can't be blank"
     end
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ''  # emailの値を空にする
-      user.valid?
-      # binding.pry で検証
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
     end 
   end
 end
 
+
+
+# 変数を受け渡す際は、beforeに定義する変数はインスタンス変数にする必要があります。
+# beforeを利用して、テストコードをさらにまとめましょう。
+# 変数userが、@userに置き換わっていることに注意
 
 
 
