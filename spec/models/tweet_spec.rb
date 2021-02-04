87cddd9b44c6741fe8c06627e2ca7dba0f5b1297
c-penguin <s.ino.p.mata.32@gmail.com>
2021-02-04 10:02:33 +0900

    記述

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,19 +8,33 @@ RSpec.describe Tweet, type: :model do
   describe 'ツイートの保存' do
     context 'ツイートが投稿できる場合' do
       it '画像とテキストがあれば投稿できる' do
+        expect(@tweet).to be_valid
       end
       it 'テキストがあれば投稿できる' do
+        @tweet.image = ''
+        expect(@tweet).to be_valid
       end
     end
     context 'ツイートが投稿できない場合' do
       it 'テキストが空では投稿できない' do
+        @tweet.text = ''
+        @tweet.valid?
+        # binding.pry
+        expect(@tweet.errors.full_messages).to include("Text can't be blank")
       end     
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @tweet.user = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('User must exist')
       end
     end
   end
 end
 
+
+
+
+# 生成した@tweetに紐づくユーザーを、nilとして無いものとしています。その上で、バリデーションを確認しています。エラーメッセージはUser must existです。
 
 
 # textにpresenceのバリデーションが設置されています。
